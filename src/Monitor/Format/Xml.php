71eb2f048a356abfb676d1b0b46359b8b5fc167a
phaniso <9phaniso9@gmail.com>
2016-02-14 23:49:35 +0100

    if cant parse xml throw exception

--- src/Monitor/Format/Xml.php
+++ src/Monitor/Format/Xml.php
@@ -12,7 +12,10 @@ class Xml implements FormatInterface
      */
     public function convertToArray($data)
     {
-        $xml = @simplexml_load_string($data);
+        $xml = simplexml_load_string($data);
+        if($xml === false) {
+            throw new \Exception('Cant\'t parse xml');
+        }
         $array = json_decode(json_encode((array) $xml), true);
         if (!is_array($array)) {
             $array = [];
